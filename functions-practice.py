0e99dd556063a986184ef6c5a34e158b1350ef63
darrenrjones <ninenumbproductions@gmail.com>
2019-02-19 12:05:31 -0800

    warm up complete

--- functions-practice.py
+++ functions-practice.py
@@ -3,26 +3,38 @@
 # LESSER OF TWO EVENS: Write a function that returns the lesser of two given numbers if both numbers are even, but returns the greater if one or both numbers are odd
 
 def lesser_of_two_evens(a,b):
-    pass
+    if a%2==0 and b%2==0:
+        return min(a,b)
+    else:
+        return max(a,b)
 
-print(lesser_of_two_evens(2,4))
-print(lesser_of_two_evens(2,5))
+# def lesser_of_two_evens(a,b):
+#     return min(a,b) if a%2==0 and b%2==0 else  max(a,b)
+
+# print(lesser_of_two_evens(2,4))
+# print(lesser_of_two_evens(2,5))
 
 # ANIMAL CRACKERS: Write a function takes a two-word string and returns True if both words begin with same letter
 
 def animal_crackers(text):
-    pass
+    text = text.split()
+    return text[0][0].lower() == text[1][0].lower()
 
-print(animal_crackers('Levelheaded Llama'))
-print(animal_crackers('Crazy Kangaroo'))
+# print(animal_crackers('Levelheaded Llama'))
+# print(animal_crackers('Crazy Kangaroo'))
 
 
 # MAKES TWENTY: Given two integers, return True if the sum of the integers is 20 or if one of the integers is 20. If not, return False
 
+# def makes_twenty(n1,n2):
+#     if n1 == 20 or n2 == 20:
+#         return True
+#     return sum((n1,n2)) == 20
 def makes_twenty(n1,n2):
-    pass
+    return n1 == 20 or n2 == 20 or sum((n1,n2)) == 20
 
 print(makes_twenty(20,10))
+print(makes_twenty(12,8))
 print(makes_twenty(2,3))
 
 
@@ -33,24 +45,24 @@ print(makes_twenty(2,3))
 def old_macdonald(name):
     pass
 
-print(old_macdonald('macdonald'))
+# print(old_macdonald('macdonald'))
 
 # MASTER YODA: Given a sentence, return a sentence with the words reversed
 
 def master_yoda(text):
     pass
 
-print(master_yoda('I am home'))
-print(master_yoda('We are ready'))
+# print(master_yoda('I am home'))
+# print(master_yoda('We are ready'))
 
 # ALMOST THERE: Given an integer n, return True if n is within 10 of either 100 or 200
 
 def almost_there(n):
     pass
 
-print(almost_there(104))
-print(almost_there(150))
-print(almost_there(209))
+# print(almost_there(104))
+# print(almost_there(150))
+# print(almost_there(209))
 
 
 #LEVEL 2 PROBLEMS
@@ -60,17 +72,17 @@ print(almost_there(209))
 def has_33(nums):
     pass
 
-print(has_33([1, 3, 3]))
-print(has_33([1, 3, 1, 3]))
-print(has_33([3, 1, 3]))
+# print(has_33([1, 3, 3]))
+# print(has_33([1, 3, 1, 3]))
+# print(has_33([3, 1, 3]))
 
 # PAPER DOLL: Given a string, return a string where for every character in the original there are three characters
 
 def paper_doll(text):
     pass
 
-print(paper_doll('Hello'))
-print(paper_doll('Mississippi'))
+# print(paper_doll('Hello'))
+# print(paper_doll('Mississippi'))
 
 
 # BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. If their sum exceeds 21 and there's an eleven, reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'
@@ -78,18 +90,18 @@ print(paper_doll('Mississippi'))
 def blackjack(a,b,c):
     pass
 
-print(blackjack(5,6,7)) #18
-print(blackjack(9,9,9)) # 'BUST'
-print(blackjack(9,9,11)) # 19
+# print(blackjack(5,6,7)) #18
+# print(blackjack(9,9,9)) # 'BUST'
+# print(blackjack(9,9,11)) # 19
 
 # SUMMER OF '69: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers.
 
 def summer_69(arr):
     pass
 
-print(summer_69([1, 3, 5])) # 9
-print(summer_69([4, 5, 6, 7, 8, 9])) # 9
-print(summer_69([2, 1, 6, 9, 11])) # 14
+# print(summer_69([1, 3, 5])) # 9
+# print(summer_69([4, 5, 6, 7, 8, 9])) # 9
+# print(summer_69([2, 1, 6, 9, 11])) # 14
 
 #CHALLENGING PROBLEMS:
 
@@ -99,9 +111,9 @@ print(summer_69([2, 1, 6, 9, 11])) # 14
 def spy_game(nums):
     pass
 
-print(spy_game([1,2,4,0,0,7,5])) # T
-print(spy_game([1,0,2,4,0,5,7])) # T
-print(spy_game([1,7,2,0,4,5,0])) # F
+# print(spy_game([1,2,4,0,0,7,5])) # T
+# print(spy_game([1,0,2,4,0,5,7])) # T
+# print(spy_game([1,7,2,0,4,5,0])) # F
 
 # COUNT PRIMES: Write a function that returns the number of prime numbers that exist up to and including a given number
 #0 and 1 not primes
@@ -110,7 +122,7 @@ print(spy_game([1,7,2,0,4,5,0])) # F
 def count_primes(num):
     pass
 
-print(count_primes(100)) #25
+# print(count_primes(100)) #25
 
 #JUST FOR FUN
 
