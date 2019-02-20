b1f9926049d9775a413e203b1bf0716bb7597e7b
darrenrjones <ninenumbproductions@gmail.com>
2019-02-20 12:03:38 -0800

    level 1 probs done

--- functions-practice.py
+++ functions-practice.py
@@ -2,11 +2,12 @@
 
 # LESSER OF TWO EVENS: Write a function that returns the lesser of two given numbers if both numbers are even, but returns the greater if one or both numbers are odd
 
-def lesser_of_two_evens(a,b):
-    if a%2==0 and b%2==0:
-        return min(a,b)
+
+def lesser_of_two_evens(a, b):
+    if a % 2 == 0 and b % 2 == 0:
+        return min(a, b)
     else:
-        return max(a,b)
+        return max(a, b)
 
 # def lesser_of_two_evens(a,b):
 #     return min(a,b) if a%2==0 and b%2==0 else  max(a,b)
@@ -15,6 +16,7 @@ def lesser_of_two_evens(a,b):
 # print(lesser_of_two_evens(2,5))
 
 # ANIMAL CRACKERS: Write a function takes a two-word string and returns True if both words begin with same letter
+
 
 def animal_crackers(text):
     text = text.split()
@@ -30,42 +32,57 @@ def animal_crackers(text):
 #     if n1 == 20 or n2 == 20:
 #         return True
 #     return sum((n1,n2)) == 20
-def makes_twenty(n1,n2):
-    return n1 == 20 or n2 == 20 or sum((n1,n2)) == 20
+def makes_twenty(n1, n2):
+    return n1 == 20 or n2 == 20 or sum((n1, n2)) == 20
 
-print(makes_twenty(20,10))
-print(makes_twenty(12,8))
-print(makes_twenty(2,3))
+# print(makes_twenty(20,10))
+# print(makes_twenty(12,8))
+# print(makes_twenty(2,3))
 
 
-#LEVEL 1 PROBLEMS
+# LEVEL 1 PROBLEMS
 
 # OLD MACDONALD: Write a function that capitalizes the first and fourth letters of a name
 
+# def old_macdonald(name):
+#     capVersion = ''
+#     for index,letter in enumerate(name):
+#         if index == 0 or index == 3:
+#             capVersion += letter.upper()
+#         else:
+#             capVersion += letter
+#     return capVersion
 def old_macdonald(name):
-    pass
+    return name[:3].capitalize() + name[3:].capitalize()
 
 # print(old_macdonald('macdonald'))
 
+
 # MASTER YODA: Given a sentence, return a sentence with the words reversed
 
+# def master_yoda(text):
+#     splitReversed = text.split()[::-1]
+#     return " ".join(splitReversed)
 def master_yoda(text):
-    pass
+    return ' '.join(text.split()[::-1])
 
 # print(master_yoda('I am home'))
 # print(master_yoda('We are ready'))
 
+
 # ALMOST THERE: Given an integer n, return True if n is within 10 of either 100 or 200
 
 def almost_there(n):
-    pass
+    # return abs(100 - n) in range(0,10) or abs(200 - n) in range(0,10)
+    return abs(100 - n) <= 10 or abs(200 - n) <= 10
 
-# print(almost_there(104))
-# print(almost_there(150))
-# print(almost_there(209))
+print(almost_there(104))  # T
+print(almost_there(104))  # T
+print(almost_there(150))  # F
+print(almost_there(209))  # T
 
 
-#LEVEL 2 PROBLEMS
+# LEVEL 2 PROBLEMS
 
 # FIND 33: Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.
 
@@ -78,6 +95,7 @@ def has_33(nums):
 
 # PAPER DOLL: Given a string, return a string where for every character in the original there are three characters
 
+
 def paper_doll(text):
     pass
 
@@ -87,7 +105,7 @@ def paper_doll(text):
 
 # BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. If their sum exceeds 21 and there's an eleven, reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'
 
-def blackjack(a,b,c):
+def blackjack(a, b, c):
     pass
 
 # print(blackjack(5,6,7)) #18
@@ -96,6 +114,7 @@ def blackjack(a,b,c):
 
 # SUMMER OF '69: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers.
 
+
 def summer_69(arr):
     pass
 
@@ -103,7 +122,7 @@ def summer_69(arr):
 # print(summer_69([4, 5, 6, 7, 8, 9])) # 9
 # print(summer_69([2, 1, 6, 9, 11])) # 14
 
-#CHALLENGING PROBLEMS:
+# CHALLENGING PROBLEMS:
 
 # SPY GAME: Write a function that takes in a list of integers and returns True if it contains 007 in order
 
@@ -116,7 +135,7 @@ def spy_game(nums):
 # print(spy_game([1,7,2,0,4,5,0])) # F
 
 # COUNT PRIMES: Write a function that returns the number of prime numbers that exist up to and including a given number
-#0 and 1 not primes
+# 0 and 1 not primes
 
 
 def count_primes(num):
@@ -124,22 +143,17 @@ def count_primes(num):
 
 # print(count_primes(100)) #25
 
-#JUST FOR FUN
+# JUST FOR FUN
 
 # PRINT BIG: Write a function that takes in a single letter, and returns a 5x5 representation of that letter
 # print_big('a')
 
-# out:   *  
+# out:   *
 #       * *
 #      *****
 #      *   *
 #      *   *
 
+
 def print_big(letter):
     pass
-
-
-
-
-
-
