05a9eeb35b8a46da6957b0d407357ead7e3f03d8
darrenrjones <ninenumbproductions@gmail.com>
2019-03-06 06:37:14 -0800

    completed

--- functions-homework-L51.py
+++ functions-homework-L51.py
@@ -1,12 +1,16 @@
 # Write a function that computes the volume of a sphere given its radius.
 
+import string
 import math
+
+
 def vol(rad):
     return 4/3 * math.pi * rad**3
 
 # print(vol(3))
 
 # Write a function that checks whether a number is in a given range(Inclusive of high and low)
+
 
 def ran_check(num, low, high):
     return num in range(low, high+1)
@@ -17,7 +21,7 @@ def ran_check(num, low, high):
 # Write a Python function that accepts a string and calculate the number of upper case letters and lower case letters.
 
 # Sample String : 'Hello Mr. Rogers, how are you this fine Tuesday?'
-# Expected Output : 
+# Expected Output :
 # No. of Upper case characters : 4
 # No. of Lower case Characters : 33
 
@@ -34,8 +38,8 @@ def up_low(s):
         else:
             pass
 
-    print(f"No. of Upper case characters : {upperCount}")
-    print(f"No. of Lower case characters : {lowerCount}")
+    # print(f"No. of Upper case characters : {upperCount}")
+    # print(f"No. of Lower case characters : {lowerCount}")
 
 
 # print(up_low('Hello Mr. Rogers, how are you this fine Tuesday?'))
@@ -46,10 +50,17 @@ def up_low(s):
 # Sample List : [1,1,1,1,2,2,3,3,3,3,4,5]
 # Unique List : [1, 2, 3, 4, 5]
 
-def unique_list(l):
-    pass
+# def unique_list(l):
+#     return list(set(l))
 
-# unique_list([1,1,1,1,2,2,3,3,3,3,4,5]) #[1, 2, 3, 4, 5]
+def unique_list(l):
+    unique = []
+    for n in l:
+        if n not in unique:
+            unique.append(n)
+    return unique
+
+# print(unique_list([1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 5]))  # [1, 2, 3, 4, 5]
 
 
 # Write a Python function to multiply all the numbers in a list.
@@ -57,11 +68,14 @@ def unique_list(l):
 # Sample List : [1, 2, 3, -4]
 # Expected Output : -24
 
-def multiply(numbers):  
-    pass
+def multiply(numbers):
+    total = 1
+    for n in numbers:
+        total *= n
+    return total
 
-# multiply([1,2,3,-4])
-# -24
+
+# print(multiply([1, 2, 3, -4]))  # -24
 
 
 # Write a Python function that checks whether a passed string is palindrome or not.
@@ -69,10 +83,13 @@ def multiply(numbers):
 # Note: A palindrome is word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.
 
 def palindrome(s):
-    pass
+    s = s.replace(' ', '')
+    return s == s[::-1]
 
-# palindrome('helleh')
-# True
+
+# print(palindrome('helleh'))  # True
+# print(palindrome('staples'))  # False
+# print(palindrome('a man a plan a canal panama'))  # True
 
 
 # Write a Python function to check whether a string is pangram or not.
@@ -82,12 +99,12 @@ def palindrome(s):
 
 # Hint: Look at the string module
 
-import string
 
-def ispangram(str1, alphabet=string.ascii_lowercase):  
-    pass
+def ispangram(str1, alphabet=string.ascii_lowercase):
+    return set(alphabet) <= set(str1.lower())
+     
 
-# ispangram("The quick brown fox jumps over the lazy dog")
-# True
-string.ascii_lowercase
-# 'abcdefghijklmnopqrstuvwxyz'
+
+print(ispangram("The quick brown fox jumps over the lazy dog")) # True
+# string.ascii_lowercase 
+# # 'abcdefghijklmnopqrstuvwxyz'
